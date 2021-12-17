97de98b5862190c4147e7e0c530c7f91cc49775e
heyoon2j <jyszz1201@gmail.com>
2021-12-17 17:55:01 +0900

    feat

--- aws/ServiceCatalog/boto3_example.py
+++ aws/ServiceCatalog/boto3_example.py
@@ -1,4 +1,4 @@
-import boto3
+import boto3 
 
 client = boto3.client('servicecatalog')
 
---
+++ aws/ec2/modifyAMIAttribute.py
@@ -0,0 +1,60 @@
+
+import os
+
+def modifyAmiAttribute(event, context):
+    '''
+    Lambda Function
+    AWS EC2 AMI
+    '''
+
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+        print()
+
+    response = client.modify_image_attribute(
+        Attribute='string',
+        Description={
+            'Value': 'string'
+        },
+        ImageId='string',
+        LaunchPermission={
+            'Add': [
+                {
+                    'Group': 'all',
+                    'UserId': 'string',
+                    'OrganizationArn': 'string',
+                    'OrganizationalUnitArn': 'string'
+                },
+            ],
+            'Remove': [
+                {
+                    'Group': 'all',
+                    'UserId': 'string',
+                    'OrganizationArn': 'string',
+                    'OrganizationalUnitArn': 'string'
+                },
+            ]
+        },
+        OperationType='add'|'remove',
+        ProductCodes=[
+            'string',
+        ],
+        UserGroups=[
+            'string',
+        ],
+        UserIds=[
+            'string',
+        ],
+        Value='string',
+        DryRun=True|False,
+        OrganizationArns=[
+            'string',
+        ],
+        OrganizationalUnitArns=[
+            'string',
+        ]
+    )
+
+    return 
