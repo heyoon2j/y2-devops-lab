aec749f43640b2183db7da49beb68dc3acaa5417
heyoon2j <jyszz1201@gmail.com>
2022-10-24 17:54:01 +0900

    feat: alb

--- terraform/aws/computing/alb_template.tf
+++ terraform/aws/computing/alb_template.tf
@@ -22,84 +22,115 @@
 'Load Balancer Resource'
 
 Args:
-    description
-        description = "Description"
+    name
+        description = "LB Name"
         type = string
-        default = "Transit Gateway"
+        #default = "Transit Gateway"
         #validation { 10.0.0.0/16, 172.16.30.0/24 ... }
 
-    amazon_side_asn
-        description = "AWS ASN"
-        type = number
-        default = 64512
-        validation { 64512 ~ 65534, 4200000000 ~ 4294967294 }
+    internal
+        description = "Internal or Internet-facing"
+        type = bool
+        default = true
+        validation { true, false }
 
-    auto_accept_shared_attachments
-        description = "auto acception about shared account"
+    load_balancer_type
+        description = "Load Balancer Type"
         type = string
-        default = "disable"
-        validation { "disable" (Default), "enable" }
+        default = "application"
+        validation { "application" (Default), "gateway", "network" }
 
-    default_route_table_association 
-        description = "default rout table associate to tgw"
-        type = string
-        default = "disable"
-        validation { "enable" (Default), "disable" }
+    subnets            = [for subnet in aws_subnet.public : subnet.id]
 
-    default_route_table_propagation
-        description = "default rout table propagate to tgw"
+    ip_address_type
+        description = "IP Address Type"
         type = string
-        default = "disable"
-        validation { "enable" (Default), "disable" }
+        default = "ipv4"
+        validation { "ipv4", "dualstack" }
 
-    dns_support
-        description = "DNS Support"
-        type = string
-        default = "enable"
-        validation { "enable" (Default), "disable" }
+    security_groups    = [aws_security_group.lb_sg.id]
 
-    vpn_ecmp_support
-        description = "VPN ECMP Routing Support"
-        type = string
-        default = "enable"
-        validation { "enable" (Default), "disable" }
+    enable_deletion_protection = true
+        description = "Is nable to delete LB (Protection Setting)"
+        type = bool
+        default = true
+        validation { true, false (Default) }
 
-    multicast_support
-        description = "Multicast Support"
+    # Application Option
+    idle_timeout = 60
+        description = "Load Balancer Type"
         type = string
-        default = "disable"
-        validation { "enable", "disable" (Disable) }
+        default = "application"
+        validation { "application" (Default), "gateway", "network" }
+
+    enable_http2
+        description = "Is enable to use HTTP2"
+        type = bool
+        default = true
+        validation { true (Default), false }
+
+    drop_invalid_header_fields
+        description = "Drop invalid header fields"
+        type = bool
+        default = false
+        validation { true, false (Default) }
+
+    preserve_host_header
+        description = "Preserve host header"
+        type = bool
+        default = false
+        validation { true, false (Default) }
+    
+    desync_mitigation_mode
+        description = "Mode setting for HTTP Desync"
+        type = string
+        default = "defensive"
+        validation { "monitor", "defensive" (Default), "strictest" }
+
+    enable_waf_fail_open
+        description = "Open when WAF is failed"
+        type = bool
+        default = false
+        validation { true, false (Default) }
+
+    #access_logs
+    bucket
+        description = "For access log, S3 bucket ID"
+        type = string
+        #default = 
+        #validation { }
+
+    prefix  = "test-lb"
+        description = "For access log, log file's prefix name"
+        type = string
+        #default = 
+        #validation { }
+
+    enabled
+        description = "Boolean to enable access_logs"
+        type = bool
+        default = false
+        validation { true, false (Default) }
 */
 
 resource "aws_lb" "alb-proj-temp" {
     name               = "alb-proj-temp"
     internal           = true
-    true, false
     load_balancer_type = "application"
-    "application" (Default), "gateway", "network"
     subnets            = [for subnet in aws_subnet.public : subnet.id]
     ip_address_type = "ipv4"
-    "ipv4", "dualstack"
 
     security_groups    = [aws_security_group.lb_sg.id]
 
     enable_deletion_protection = true
-    true, false (Default)
 
     # Application Option
     idle_timeout = 60
     enable_http2 = true
-    true (Default) / false
     drop_invalid_header_fields = false
-    true, false (Default)
     preserve_host_header = false
-    true, false (Default)
-    
     desync_mitigation_mode = "defensive"
-    "monitor", "defensive" (Default), "strictest"
-
     enable_waf_fail_open = false
-    true, false (Default)
 
     access_logs {
         bucket  = aws_s3_bucket.lb_logs.bucket
@@ -111,3 +142,85 @@ resource "aws_lb" "alb-proj-temp" {
         Environment = "production"
     }
 }
+
+
+
+/*
+'Target Group Resource'
+
+Args:
+    name
+        description = "LB Name"
+        type = string
+        #default = "Transit Gateway"
+        #validation { 10.0.0.0/16, 172.16.30.0/24 ... }
+
+    internal
+        description = "Internal or Internet-facing"
+        type = bool
+        default = true
+        validation { true, false }
+
+    load_balancer_type
+        description = "Load Balancer Type"
+        type = string
+        default = "application"
+        validation { "application" (Default), "gateway", "network" }
+
+    subnets            = [for subnet in aws_subnet.public : subnet.id]
+*/
+
+# Instance Type
+resource "aws_lb_target_group" "test" {
+    name     = "tf-example-lb-tg"
+    target_type = "instance"
+    port     = 80
+    protocol = "HTTP"
+    "HTTP", "HTTPS", "GRPC"
+    vpc_id   = aws_vpc.main.id
+
+    #ALB Option
+    deregistration_delay = 300
+    0 - 3600, 300 (Default)
+    load_balancing_algorithm_type = "round_robin"
+    "round_robin", "least_outstanding_requests"
+    protocol_version = "HTTP1"
+    "HTTP1", "HTTP2", ""
+
+
+
+
+    #NLB Option
+    connection_termination = false
+    true, false (Default)
+    deregistration_delay = 300
+    preserve_client_ip = true
+    true (Default), false
+
+
+
+
+
+    health_check = {
+
+    }
+}
+
+# IP Type
+resource "aws_lb_target_group" "ip-example" {
+    name        = "tf-example-lb-tg"
+    target_type = "ip"
+    "instance", "ip", "lambda", "alb"
+    ip_address_type = "ipv4"
+    "ipv4", "ipv6"
+    port        = 80
+    protocol    = "HTTP"
+    vpc_id      = aws_vpc.main.id
+}
+
+
+
+
+
+
+
