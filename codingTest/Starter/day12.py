ab7986a6192f2578fe22c56c6bbdf4fa4387b8df
heyoon2j <jyszz1201@gmail.com>
2022-12-28 18:02:12 +0900

    test: Day12 init

---
+++ codingTest/Starter/day12.py
@@ -0,0 +1,30 @@
+"""
+모음 제거
+"""
+def solution(my_string):
+    a = ['a', 'e', 'i', 'o', 'u']
+    for i in a:
+        my_string = my_string.replace(a[i], '')
+    return my_string
+
+print(solution("bus"))
+
+
+"""
+문자열 정렬하기 (1)
+"""
+
+
+
+"""
+숨어있는 숫자의 덧셈 (1)
+"""
+
+
+
+"""
+소인수분해
+"""
+
+
+
