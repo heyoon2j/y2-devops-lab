dc963a99acfefbf4f3af87a5beca2ba789f38cfd
heyoon2j <jyszz1201@gmail.com>
2023-01-02 03:05:13 +0900

    test: coding test - Starter

--- codingTest/Starter/day16.py
+++ codingTest/Starter/day16.py
@@ -29,5 +29,11 @@ def solution(my_string):
 print(solution("100 + 500"))
 
 """
-
+배열의 유사도
 """
+def solution(s1, s2):
+    answer = 0
+    for i in s1:
+        if i in s2:
+            answer += 1
+    return answer
